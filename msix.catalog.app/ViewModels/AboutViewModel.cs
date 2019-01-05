38c02e27515c8d26675612586369581e063cb3ee
Rido ___ <ridomin@live.com>
2019-01-05 01:30:48 +0000

    Merged PR 19: Move Winmd ref to lib
    Move Winmd ref to lib

--- msix.catalog.app/ViewModels/AboutViewModel.cs
+++ msix.catalog.app/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using msix_catalog_lib;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,108 +14,13 @@ namespace msix_catalog_app.ViewModels
 {
     public class AboutViewModel
     {
-        public string PublisherDisplayName => GetSafePublisherDisplayName();
-
-        private string GetSafePublisherDisplayName()
-        {
-            if (ExecutionMode.IsRunningAsUwp())
-            {
-                return Package.Current.PublisherDisplayName;
-            }
-            return "Not Packaged";
-        }
-
-        public string InstallLocation => System.Reflection.Assembly.GetExecutingAssembly().Location;
-
-        public string VersionString => GetSafePackageVersion();
-
-        private string GetSafePackageVersion()
-        {
-            var result = "0";
-            if (ExecutionMode.IsRunningAsUwp())
-            {
-                result = string.Format("{0}.{1}.{2}.{3}",
-                                                           Package.Current.Id.Version.Major,
-                                                           Package.Current.Id.Version.Minor,
-                                                           Package.Current.Id.Version.Build,
-                                                           Package.Current.Id.Version.Revision);
-
-            }
-            return result;
-        }
-
-        public string MyVersion => this.GetType().AssemblyQualifiedName;
-
-        public string Metadata => GetSafeMetadata();
-
-
-        private string GetSafeMetadata()
-        {
-            if (ExecutionMode.IsRunningAsUwp())
-            {
-                var path = Path.Combine(Package.Current.InstalledLocation.Path, "AppxManifest.xml");
-                XDocument doc = XDocument.Load(path, LoadOptions.None);
-                var xname = XNamespace.Get("http://schemas.microsoft.com/developer/appx/2015/build");
-                var metadata = doc.Descendants(xname + "Metadata").First();
-                var res = new StringBuilder();
-                foreach (XElement n in metadata.Elements())
-                {
-                    res.Append(n.Attribute("Name").Value + ":");
-                    res.Append(n.Attribute("Value")?.Value);
-                    res.Append(n.Attribute("Version")?.Value + "\r\n");
-                }
-                return res.ToString();
-            }
-            return string.Empty;
-        }
-
-        public string StoreInfo => GetStoreInfo();
-
-        public static string GetStoreInfo()
-        {
-            var res = "Store API not available ";
-            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Services.Store.StoreContext"))
-            {
-                var ctx = Windows.Services.Store.StoreContext.GetDefault();
-                var lic = ctx.GetStoreProductForCurrentAppAsync().AsTask().Result;
-
-
-                if (lic == null)
-                {
-                    res = "License is null";
-                }
-                else if (lic.Product == null)
-                {
-                    res = "License Product is null:" + lic.ExtendedError.Message;
-                }
-                else
-                {
-                    res = lic.Product.LinkUri.ToString();
-                }
-            }
-            return res;
-        }
-
-        public string InstalledOn
-        {
-            get
-            {
-                return SafeInstalledOn();
-            }
-        }
-
-        private static string SafeInstalledOn()
-        {
-            DateTimeOffset installed;
-            try
-            {
-                installed = Package.Current.InstalledDate;
-            }
-            catch (Exception)
-            {
-                installed = DateTime.Now;
-            }
-            return installed.ToLocalTime().ToString();
-        }
+        public string PublisherDisplayName => ThisAppVersionInfo.PublisherDisplayName;
+        public string InstallLocation => ThisAppVersionInfo.InstallLocation;
+        public string VersionString => ThisAppVersionInfo.VersionString;
+        public string MyVersion => ThisAppVersionInfo.MyVersion;
+        public string Metadata => ThisAppVersionInfo.Metadata;
+        public string StoreInfo => ThisAppVersionInfo.StoreInfo;
+        public string InstalledOn => ThisAppVersionInfo.InstalledOn;
+        public string DotNetFlavor => ThisAppVersionInfo.DotNetFlavor;
     }
 }
---
+++ msix.catalog.lib/ThisAppVersionInfo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Windows.ApplicationModel;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace msix_catalog_lib
+{
+    public class ThisAppVersionInfo
+    {
+        public static string PublisherDisplayName => GetSafePublisherDisplayName();
+
+        private static string GetSafePublisherDisplayName()
+        {
+            if (OSVersionHelper.WindowsVersionHelper.HasPackageIdentity)
+            {
+                return Package.Current.PublisherDisplayName;
+            }
+            return "Not Packaged";
+        }
+
+        public static string InstallLocation => System.Reflection.Assembly.GetCallingAssembly().Location;
+
+        public static string VersionString => GetSafePackageVersion();
+
+        private static string GetSafePackageVersion()
+        {
+            var result = "0";
+            if (OSVersionHelper.WindowsVersionHelper.HasPackageIdentity)
+            {
+                result = string.Format("{0}.{1}.{2}.{3}",
+                                                           Package.Current.Id.Version.Major,
+                                                           Package.Current.Id.Version.Minor,
+                                                           Package.Current.Id.Version.Build,
+                                                           Package.Current.Id.Version.Revision);
+
+            }
+            return result;
+        }
+
+        public static string MyVersion => Assembly.GetCallingAssembly().GetName().FullName;
+
+        public static string Metadata => GetSafeMetadata();
+
+
+        private static string GetSafeMetadata()
+        {
+            if (OSVersionHelper.WindowsVersionHelper.HasPackageIdentity)
+            {
+                var path = Path.Combine(Package.Current.InstalledLocation.Path, "AppxManifest.xml");
+                XDocument doc = XDocument.Load(path, LoadOptions.None);
+                var xname = XNamespace.Get("http://schemas.microsoft.com/developer/appx/2015/build");
+                var metadata = doc.Descendants(xname + "Metadata").First();
+                var res = new StringBuilder();
+                foreach (XElement n in metadata.Elements())
+                {
+                    res.Append(n.Attribute("Name").Value + ":");
+                    res.Append(n.Attribute("Value")?.Value);
+                    res.Append(n.Attribute("Version")?.Value + "\r\n");
+                }
+                return res.ToString();
+            }
+            return string.Empty;
+        }
+
+        public static string StoreInfo => GetStoreInfo();
+
+        public static string GetStoreInfo()
+        {
+            var res = "Store API not available ";
+            //if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Services.Store.StoreContext"))
+            //{
+            //    var ctx = Windows.Services.Store.StoreContext.GetDefault();
+            //    var lic = ctx.GetStoreProductForCurrentAppAsync().AsTask().Result;
+
+            //    if (lic == null)
+            //    {
+            //        res = "License is null";
+            //    }
+            //    else if (lic.Product == null)
+            //    {
+            //        res = "License Product is null:" + lic.ExtendedError.Message;
+            //    }
+            //    else
+            //    {
+            //        res = lic.Product.LinkUri.ToString();
+            //    }
+            //}
+            return res;
+        }
+
+        public static string InstalledOn => SafeInstalledOn();
+
+        private static string SafeInstalledOn()
+        {
+            DateTimeOffset installed;
+            try
+            {
+                installed = Package.Current.InstalledDate;
+            }
+            catch (Exception)
+            {
+                installed = DateTime.Now;
+            }
+            return installed.ToLocalTime().ToString();
+        }
+
+        public static string DotNetFlavor => GetDotNetFlavor();
+
+        private static string GetDotNetFlavor()
+        {
+            return typeof(string).Assembly.Location;
+        }
+    }
+}
