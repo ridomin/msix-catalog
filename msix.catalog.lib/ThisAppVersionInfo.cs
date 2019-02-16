684cb5ad30c67e75358514efa2e8db992cb3bf64
ridomin <ridomin@live.com>
2019-02-15 21:12:24 -0800

    Deployment Type

--- msix.catalog.lib/ThisAppVersionInfo.cs
+++ msix.catalog.lib/ThisAppVersionInfo.cs
@@ -68,7 +68,7 @@ namespace msix.catalog.lib
             return string.Empty;
         }
 
-        public static string StoreInfo =>GetStoreInfo();
+        //public static string StoreInfo =>GetStoreInfo();
         public static string GetStoreInfo()
         {
             var res = "Store API not available ";
